0ca96f18a200a3fe05915155a778efae25a9936d
SHASHANK GAUTAM <SHASHANK GAUTAM>
2021-12-07 16:52:20 +0530

    submit

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -9,6 +9,10 @@ import 'package:flutter/material.dart';
 --> Now 'addTx' should be set with the help of constructer of 'NewTransaction', because we are passing something to the constructer in 'user_transaction.dart'
 --> Now 'addTx' is used as a pointer function in 'onPressed' which will take paarmetrs as the value of 'title' and 'amount'
 --> Text always returns a string, so we convert it to the double as double.parse().
+--> 'keyboardType: TextInputType.' gives flexibility to choose what type of keybord should appear for data insertion,
+    here we had used number keyboard for amount entering
+--> Assigning 'titleController' and 'amtController' to 'enteredtitle' and 'enteredamt' respectively
+--> When the 'if()' condition becomes true the further function 'will not execute'
 */
 
 class NewTransaction extends StatelessWidget {
@@ -17,6 +21,17 @@ class NewTransaction extends StatelessWidget {
   final Function addTx;
 
   NewTransaction(this.addTx);
+
+  void submitData() {
+    final enteredtitle = titleController.text;
+    final enteredamt = double.parse(amtController.text);
+
+    if (enteredtitle.isEmpty || enteredamt <= 0) {
+      return;
+    }
+
+    addTx(titleController.text, double.parse(amtController.text));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,15 +45,16 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (value) => submitData,
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amtController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (value) => submitData,
             ),
             TextButton(
-              onPressed: () {
-                addTx(titleController.text, double.parse(amtController.text));
-              },
+              onPressed: submitData,
               child: Text('Add Transaction'),
               style: TextButton.styleFrom(
                   padding: EdgeInsets.all(10),
