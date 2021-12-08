f8d35c7a0d9b0760573d3d3c780783251981250a
SHASHANK GAUTAM <SHASHANK GAUTAM>
2021-12-08 14:50:06 +0530

    showModelBottemSheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, sized_box_for_whitespace, prefer_const_literals_to_create_immutables
 
-import 'package:expenses/widgets/user_transaction.dart';
+import 'package:expenses/widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
+import './models/transactions.dart';
+import 'widgets/transactions_list.dart';
 
 /*
 --> 'Column' has main axis Vertical and Cross axis horizontal, and vice-versa for Row
@@ -16,7 +18,15 @@ import 'package:flutter/material.dart';
 --> 'elevation' inside the 'Card' controls the drop shadow
 --> To make the overall content scrollable  we have to use the widget 'SingleChildScrollView()'
     i.e, just wrap the content in the SingleChildScrollView() widget
-
+--> 'actions:' is a List of Widget
+--> 'IconButtton' is type of button which returns icon, Icon buttons are commonly used in the AppBar.actions field, but they can be used in many other places as well.
+--> Inside the'IconButtton', 'icon' takes positional argument 'Icon' which takes 'Icons' which is a class. 
+--> 'FloatingActionButton' is defined under the 'scafflod' section
+--> 'floatingActionButtonLocation:' is used to set the loaction of floating Button
+--> 'showModelBottemSheet' is used when we tap a button a model sheet should appear from bottem
+--> In function 'startAddNewTransaction' we take 'ctx' as argument which will further pass as 'context' in 'showModelBottemSheet'
+--> 'builder' is a function that needs to return the widget that should be inside that 'showModalBottomSheet'
+-->
 */
 
 void main() {
@@ -27,22 +37,69 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, //used to remove the debug banner
       title: 'Daily Expenses',
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   //late String
   //  titleInput; // while using late before variables make sure that, variable must be initialized later
   //late String amtInput;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTx(String title, double amt) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: title,
+        amount: amt,
+        date: DateTime.now());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return NewTransaction(_addNewTx);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Daily Expenses'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () => _startAddNewTransaction(context),
+            icon: Icon(Icons.add),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -65,9 +122,18 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
             ),
-            UserTransaction(),
+            TransactioList(_userTransactions),
           ],
         ),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+        ),
+        backgroundColor: Colors.green,
       ),
     );
   }
